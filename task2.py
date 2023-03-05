0167427eddda5e108ae9abc0ce2ad2b7b3eef4af
Vasiliy <arapov95vasiliy@gmail.com>
2023-03-05 13:00:17 +0300

    исправил ошибку

--- task2.py
+++ task2.py
@@ -6,9 +6,14 @@ list_1 = list(map(int, input().split()))
 temp_sum = 0
 max_sum = 0
 
-for i in range(len(list_1) - 1):
+for i in range(len(list_1)):
     temp_sum = list_1[i] + list_1[i - 1] + list_1[i - 2]
     if temp_sum > max_sum:
         max_sum = temp_sum
 
 print(max_sum)
+
+
+print(len(list_1))
+print(range(len(list_1)))
+print(list_1[3])
